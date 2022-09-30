8d72dab1241381a9420e28096f49d39cb1906842
hugowxyz1 <39029526+hugowxyz@users.noreply.github.com>
2022-09-30 21:03:04 +0100

    a

--- primes.py
+++ primes.py
@@ -1,6 +1,24 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
+def isPrime(n):
+    for i in range(2, int(n**(1/2)) + 1):
+        if n % i == 0:
+            return False
+
+    return True
+
+
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError()
+
     list = []
+    counter = 2
+    while len(list) < number_of_primes:
+        if isPrime(counter):
+            list.append(counter)
+        counter += 1
+
     return list
